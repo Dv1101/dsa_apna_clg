e4e6b1f36041034f3024b5ea562819d18b02c12b
Dv1101 <dhruvvaria1101@gmail.com>
2023-02-24 21:34:59 +0530

    functions_examples

---
+++ functions/pythagorian_triplet.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+bool check(int x, int y, int z)
+{
+    int a = max(x, max(y, z));
+    int b, c;
+
+    if(a == x)
+    {
+        b=y;
+        c=z;
+    }
+    else if(a == y)
+    {
+        b=x;
+        c=z;
+    }
+    else
+    {
+        b=x;
+        c=y;
+    }
+
+    if(a*a == b*b+c*c)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int32_t main()
+{
+    int x, y, z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,x))
+    {
+        cout<<x<<" "<<y<<" "<<z<<" "<<"is a Pythagorean Triplet.";
+    }
+    else
+    {
+        cout<<x<<" "<<y<<" "<<z<<" "<<"is not a Pythagorean Triplet.";
+    }
+
+    
+    return 0;
+}
