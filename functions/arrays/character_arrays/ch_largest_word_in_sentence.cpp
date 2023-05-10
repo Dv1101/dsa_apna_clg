877108c0826de293b8e5a6f50f7c5f8d2526d880
Dv1101 <dhruvvaria1101@gmail.com>
2023-05-11 01:41:42 +0530

    nEW_2DARRAY & CHRACTER ARR

---
+++ functions/arrays/character_arrays/ch_largest_word_in_sentence.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cin.ignore();
+    
+    char arr[n+1];
+    cin.getline(arr, n);
+    cin.ignore();
+
+    int i=0; 
+    int currLen=0, maxLen=0;
+
+    while(1)
+    {
+        
+    } 
+    return 0;
+}
